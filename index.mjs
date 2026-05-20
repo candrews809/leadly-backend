f50ea8e01282917801a4fcbe4f7fc217029d23ef
candrews809 <sfgiants4cole@gmail.com>
2026-05-19 20:09:19 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -143,6 +143,7 @@ async function createCheckoutSession(plan) {
       'mode': 'subscription',
       'line_items[0][price]': priceId,
       'line_items[0][quantity]': '1',
+      'subscription_data[trial_period_days]': '30',
       'success_url': 'https://leadly-main.netlify.app?success=true',
       'cancel_url': 'https://leadly-main.netlify.app?canceled=true',
     }),
